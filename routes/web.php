39dcfcbbf0e869a1c7dcee10a46c1253ff4d1e61
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-07-17 22:04:44 +0000

    feat: scaffold project CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| These routes handle all project-related pages.
+| Resource routes automatically create the standard CRUD endpoints.
+|
+*/
+
+Route::resource('projects', ProjectController::class);
+
+// Redirect the home page to the projects list.
+Route::redirect('/', '/projects');
